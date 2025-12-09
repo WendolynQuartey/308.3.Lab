fcb3faba7ecfd6a37a05a279fba0f8ec2bdcdc1e
WendolynQuartey <quarteywendolyn@gmail.com>
2025-12-09 15:41:50 -0500

    all parts work

--- script.js
+++ script.js
@@ -6,17 +6,17 @@
 // If a number is divisible by 5, log “Buzz.”
 // If a number is divisible by both 3 and 5, log “Fizz Buzz.”
 // If a number is not divisible by either 3 or 5, log the number.
-// for (let num = 1; num <= 100; num++){
-//    if (num % 3 === 0 && num % 5 === 0){
-//       console.log(num, "Fizz Buzz");
-//    } else if (num % 5 === 0) {
-//       console.log(num, "Buzz");
-//    } else if (num % 3 === 0) {
-//       console.log(num, "Fizz");
-//    } else {
-//       console.log(num);
-//    }
-// }
+for (let num = 1; num <= 100; num++){
+   if (num % 3 === 0 && num % 5 === 0){
+      console.log(num, "Fizz Buzz");
+   } else if (num % 5 === 0) {
+      console.log(num, "Buzz");
+   } else if (num % 3 === 0) {
+      console.log(num, "Fizz");
+   } else {
+      console.log(num);
+   }
+}
 
 // Part 3 
 let csvData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26`;
@@ -30,16 +30,21 @@ let csvData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n6
 let cell = "";
 let row = "";
 
-for (let i of csvData) {
-   cell += i;
-   if (i == ','){
+for (let i = 0; i < csvData.length; i++) {
+   // console.log("csvData[i]: ", csvData[i]);
+   cell += csvData[i]; // add the value of csvData at index i 
+   // console.log("Cell: ", cell);
+   if (csvData[i] == ','){ // check if the value of  csvData at index i is equal to the chracter ','
       row += cell;
+      // console.log("Row: ", row);
       cell = "";
    }
-   else if (i == '\n'){
+   else if (csvData[i] == '\n' || i == csvData.length-1  ){
+      row += cell;
       console.log(row);
       row = "";
-   }
+      cell = "";
+   } 
 }
 
 
